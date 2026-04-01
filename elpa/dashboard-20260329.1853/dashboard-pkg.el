46851c79279c2ef62e39b923881c2aef3a08d805
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2026-04-01 23:26:40 +0200

    first commit

---
+++ elpa/dashboard-20260329.1853/dashboard-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dashboard" "20260329.1853"
+  "A startup screen extracted from Spacemacs."
+  '((emacs "27.1"))
+  :url "https://github.com/emacs-dashboard/dashboard"
+  :commit "1c21c2586da32180f921df812a9276ab3ef18c17"
+  :revdesc "1c21c2586da3"
+  :keywords '("startup" "screen" "tools" "dashboard")
+  :authors '(("Rakan Al-Hneiti" . "rakan.alhneiti@gmail.com"))
+  :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")
+                 ("Ricardo Arredondo" . "ricardo.richo@gmail.com")))
